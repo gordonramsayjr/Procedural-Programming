fc88a4d0d1ad4be4274d90dca507a8c127adcaf5
gordonramsayjr <66240585+gordonramsayjr@users.noreply.github.com>
2021-03-18 00:08:46 +1100

    Update Plotting.py

--- Plotting.py
+++ Plotting.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt               # import matplotlib and numpy libraries
 import numpy as np
 
 
-t = np.linspace(0,np.pi,500)
-y = 2*np.sin(5*t+np.pi)
+t = np.linspace(0,np.pi,500)                  # defines t as an array that has 500 values between 0 and pi.
+y = 2*np.sin(5*t+np.pi)                       # equation to get y
 
-plt.plot(t,y)
-plt.title("Oscillation One Vs. Time")
-plt.show()
+plt.plot(t,y)                                 # plots all t and y elements
+plt.title("Oscillation One Vs. Time")         # defines graph title
+plt.show()                                    # shows graph
